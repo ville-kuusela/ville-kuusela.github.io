ec62ddba72e31bdee68863563d4ba8d77c12fce6
ville-kuusela <villekuusela19@gmail.com>
2026-04-30 11:30:47 +0300

    Update about section text

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -8,10 +8,10 @@ const AboutSection = () => {
       </div>
       <div className="space-y-4 text-sm text-off-black leading-relaxed">
         <p>
-          Hei! Olen etenkin frontendiin erikoistunut JavaScript-ohjelmistokehittäjä Jyväskylästä. Työkokemukseni ja opintojeni aikana olen kerryttänyt monipuolista osaamista nopeiden ja laadukkaiden verkko- ja mobiilisovellusten luomisesta. Hallitsen tekoälytyökalujen, kuten GitHub Copilot ja Claude, käytön työskentelyni tehostamisessa.
+          Hei! Olen JavaScript-kehittämiseen erikoistunut ohjelmistokehittäjä Jyväskylästä. Työkokemukseni ja opintojeni aikana olen kerryttänyt monipuolista osaamista nopeiden ja laadukkaiden verkko- ja mobiilisovellusten luomisesta. Hallitsen tekoälytyökalujen, kuten GitHub Copilotin ja Clauden, käytön työskentelyni tehostamisessa.
         </p>
         <p>
-          Minulla on eniten kokemusta Vuella ja Reactilla kehittämisestä, mukaan lukien Nuxt ja Next.js. Olen perehtynyt myös Laravel-ekosysteemin, jonka kautta olen päässyt sukeltamaan myös backendin ja tietokantojen maailmaan.
+          Minulla on kokemusta monista JavaScript-frameworkeista, etenkin Vue/Nuxt ja React/React Native/Next.js. Olen perehtynyt myös Laravel-ekosysteemin, jonka kautta olen päässyt sukeltamaan myös backendin ja tietokantojen maailmaan.
         </p>
         <p>
           Ohjelmistokehittäjänä tärkeimmät arvoni ovat luotettavuus ja laadukkuus. Minulle on tärkeää että työni jälki on hyvää ja koodini on tasokasta. Työskentelen tehokkaasti sekä tiimien jäsenenä että myös itsenäisesti.
